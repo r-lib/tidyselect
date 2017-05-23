767d29c44d22946bd7a5adab4ba345069dbe8c2e
Lionel Henry <lionel.hry@gmail.com>
2017-05-23 20:06:22 +0200

    Remove tbl_at_vars() and tbl_if_vars() tests

--- tests/testthat/test-select-helpers.R
+++ tests/testthat/test-select-helpers.R
@@ -250,27 +250,3 @@ test_that("rename_vars() expects symbol or string", {
     fixed = TRUE
   )
 })
-
-
-
-# tbl_at_vars -------------------------------------------------------------
-
-test_that("tbl_at_vars() errs on bad input", {
-  expect_error(
-    tbl_at_vars(iris, raw(3)),
-    "`.vars` must be a character/numeric vector or a `vars()` object, not raw",
-    fixed = TRUE
-  )
-})
-
-
-
-# tbl_if_vars -------------------------------------------------------------
-
-test_that("tbl_if_vars() errs on bad input", {
-  expect_error(
-    tbl_if_vars(iris, funs(identity, force), environment()),
-    "`.predicate` must have length 1, not 2",
-    fixed = TRUE
-  )
-})
