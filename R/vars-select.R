82e090b83a1a7a97d980c2b4f90afd4f745f1f52
Lionel Henry <lionel.hry@gmail.com>
2017-08-22 17:20:25 +0200

    Revert semantics for context expressions as well

--- R/vars-select.R
+++ R/vars-select.R
@@ -142,13 +142,8 @@ vars_select <- function(.vars, ..., .include = character(), .exclude = character
   first <- f_rhs(quos[[1]])
   initial_case <- if (is_negated(first)) list(seq_along(.vars)) else integer(0)
 
-  # Evaluate symbols in an environment where columns are bound, but
-  # not calls (select helpers are scoped in the calling environment).
-  is_helper <- map_lgl(quos, quo_is_helper)
-  ind_list <- map_if(quos, is_helper, eval_tidy)
-
   data <- set_names(as.list(seq_along(.vars)), .vars)
-  ind_list <- map_if(ind_list, !is_helper, eval_tidy, data)
+  ind_list <- map(quos, eval_tidy, data)
 
   ind_list <- c(initial_case, ind_list)
   names(ind_list) <- c(names2(initial_case), names2(quos))
