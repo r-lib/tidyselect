5aae9fdecf7b5704be8821b28275afb33f4a1a10
Lionel Henry <lionel.hry@gmail.com>
2019-09-24 10:27:09 +0200

    Use "must" form in error messages

--- R/vars-select.R
+++ R/vars-select.R
@@ -350,7 +350,7 @@ inds_check <- function(x, vars, incl, dups, unrenamed, unrenamed_vars) {
       glue::glue("* Columns {cols} are being renamed to `{dup}`.")
     })
     msg <- paste_line(
-      "Can't rename different columns to the same column name.",
+      "Must use different names when renaming columns.",
       !!!probs
     )
 
@@ -365,7 +365,7 @@ inds_check <- function(x, vars, incl, dups, unrenamed, unrenamed_vars) {
       glue::glue("* Column `{col}` is being renamed to existing column `{dup}`.")
     })
     msg <- paste_line(
-      "Can't rename column to an existing column name.",
+      "Must use new name when renaming columns.",
       !!!probs
     )
 
