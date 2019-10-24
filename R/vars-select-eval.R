7e0e5d653d411d90b7e2657330f079fb47a5d2bf
Lionel Henry <lionel.hry@gmail.com>
2019-10-24 18:09:32 +0200

    Improve external vector note
    Co-Authored-By: Hadley Wickham <h.wickham@gmail.com>

--- R/vars-select-eval.R
+++ R/vars-select-eval.R
@@ -287,7 +287,7 @@ eval_sym <- function(name, data_mask, context_mask, colon = FALSE) {
       inform(glue_c(
         "Note: Using an external vector in selections is brittle.",
         i = "If the data contains `{name}` it will be selected instead.",
-        i = "Use `all_of({name})` instead of just `{name}` to silence this message."
+        i = "Use `all_of({name})` instead of `{name}` to silence this message."
       ))
     }
 
