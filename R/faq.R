9d82f5380a317299ccaaa70d2d01dc83dcc59e55
Lionel Henry <lionel.hry@gmail.com>
2019-11-15 17:15:02 +0100

    Remove `@description` tags

--- R/faq.R
+++ R/faq.R
@@ -1,7 +1,5 @@
 #' Error: Must be used within a *selecting* function
 #'
-#' @description
-#'
 #' @includeRmd man/faq/selection-context.Rmd
 #'
 #' @name faq-selection-context
@@ -9,8 +7,6 @@
 NULL
 
 #' Note: Using an external vector in selections is brittle
-#'
-#' @description
 #'
 #' @includeRmd man/faq/external-vector.Rmd
 #'
