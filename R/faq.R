84bba741091b8fba231a774cb1d822bc6484d91a
Lionel Henry <lionel.hry@gmail.com>
2019-11-15 12:29:47 +0100

    Add faq for external vectors

--- R/faq.R
+++ R/faq.R
@@ -7,3 +7,13 @@
 #' @family faq
 #' @name faq-selection-context
 NULL
+
+#' Note: Using an external vector in selections is brittle
+#'
+#' @description
+#'
+#' @includeRmd man/faq/external-vector.Rmd
+#'
+#' @family faq
+#' @name faq-external-vector
+NULL
