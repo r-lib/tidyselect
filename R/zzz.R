c10154dedc91946f088249544fac953bb5486946
Lionel Henry <lionel.hry@gmail.com>
2021-04-29 13:31:48 +0200

    Remove examples that unexpectedly mutated the base env

--- R/zzz.R
+++ R/zzz.R
@@ -7,25 +7,6 @@
 #'
 #' @export
 #' @keywords internal
-#' @examples
-#' # You can easily embed the helpers by burying them in the scopes of
-#' # input quosures. For this example we need an environment where
-#' # tidyselect is not attached:
-#' local(envir = baseenv(), {
-#'   vars <- c("foo", "bar", "baz")
-#'   helpers <- tidyselect::vars_select_helpers
-#'
-#'   my_select <- function(...) {
-#'     quos <- rlang::quos(...)
-#'     quos <- lapply(quos, rlang::env_bury, !!! helpers)
-#'
-#'     tidyselect::vars_select(vars, !!! quos)
-#'   }
-#'
-#'   # The user can now call my_select() with helpers without having
-#'   # to attach tidyselect:
-#'   my_select(starts_with("b"))
-#' })
 vars_select_helpers <- list(
   starts_with = starts_with,
   ends_with = ends_with,
