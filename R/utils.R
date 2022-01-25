a9687be059cdffe341a538681b0dfeed21c7434a
Lionel Henry <lionel.hry@gmail.com>
2022-01-25 15:03:52 +0100

    Move `select_loc()` and `rename_loc()` to the namespace

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,38 @@
+select_loc <- function(x,
+                       sel,
+                       ...,
+                       include = NULL,
+                       exclude = NULL,
+                       strict = TRUE,
+                       name_spec = NULL,
+                       allow_rename = TRUE) {
+  ellipsis::check_dots_empty()
+
+  eval_select(
+    enquo(sel),
+    x,
+    include = include,
+    exclude = exclude,
+    strict = strict,
+    name_spec = name_spec,
+    allow_rename = allow_rename
+  )
+}
+
+rename_loc <- function(x,
+                       sel,
+                       ...,
+                       strict = TRUE,
+                       name_spec = NULL) {
+  ellipsis::check_dots_empty()
+  rename_impl(
+    x,
+    names(x),
+    enquo(sel),
+    strict = strict,
+    name_spec = name_spec
+  )
+}
 
 sym_dollar <- quote(`$`)
 sym_brackets2 <- quote(`[[`)
