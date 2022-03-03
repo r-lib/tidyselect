1d242de816c980382d76c97e921c9c8ea1e968de
Lionel Henry <lionel.hry@gmail.com>
2022-03-03 09:15:07 +0100

    Apply `usethis::use_package_doc()`

--- R/tidyselect-package.R
+++ R/tidyselect-package.R
@@ -1,6 +1,10 @@
+#' @keywords internal
 #' @import rlang
 #' @importFrom glue glue
 #' @importFrom purrr discard map map_chr map_if map_lgl map2 map2_chr
 #'   detect_index negate walk every compact
-#' @keywords internal
 "_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
