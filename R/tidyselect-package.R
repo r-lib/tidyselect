f0048bf478f9173998834de1c63fd67ecd7d9e3f
Lionel Henry <lionel.hry@gmail.com>
2022-03-03 11:55:28 +0100

    Drop purrr dependency in favour of `compat-purrr.R`
    Closes #216

--- R/tidyselect-package.R
+++ R/tidyselect-package.R
@@ -1,8 +1,6 @@
 #' @keywords internal
 #' @import rlang
 #' @importFrom glue glue
-#' @importFrom purrr discard map map_chr map_if map_lgl map2 map2_chr
-#'   detect_index negate walk every compact
 "_PACKAGE"
 
 ## usethis namespace: start
