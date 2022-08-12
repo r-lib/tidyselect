30cd103b52b43ae73e99d3a6cd64e145276e6056
Hadley Wickham <h.wickham@gmail.com>
2022-08-12 06:24:41 -0500

    Follow up to #275

--- R/helpers-pattern.R
+++ R/helpers-pattern.R
@@ -150,13 +150,15 @@ matches <- function(match,
   if (inherits(match, "pattern") || inherits(match, "stringr_pattern")) {
     check_installed("stringr")
     if (!missing(ignore.case)) {
-      cli::cli_abort("{.arg ignore.case} not supported when {.arg match} is a stringr pattern.")
+      cli::cli_abort("{.arg ignore.case} not supported when {.arg match} is a {.pkg stringr} pattern.")
     }
     if (!missing(perl)) {
-      cli::cli_abort("{.arg perl} not supported when {.arg match} is a stringr pattern.")
+      cli::cli_abort("{.arg perl} not supported when {.arg match} is a {.pkg stringr} pattern.")
     }
+
+    # no [ or [[ methods for pattern objects
     if (length(match) > 1) {
-      cli::cli_abort("stringr patterns must be length 1.")
+      cli::cli_abort("{.pkg stringr} patterns must be length 1.")
     }
 
     stringr::str_which(vars, match)
