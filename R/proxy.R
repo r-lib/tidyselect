06165d6fd361ad83c84194be14b70ae87bc5370b
Hadley Wickham <h.wickham@gmail.com>
2022-09-13 15:27:53 -0500

    Apply suggestions from code review
    Co-authored-by: Davis Vaughan <davis@rstudio.com>

--- R/proxy.R
+++ R/proxy.R
@@ -2,16 +2,18 @@
 #'
 #' * `tidyselect_data_proxy()` should return a 0-row data frame.
 #' * `tidyselect_data_supports_predicates()` should return `FALSE` if
-#'    `tidyselect_data_proxy()` doesn't return actual types.
+#'   `tidyselect_data_proxy()` doesn't return actual types.
 #'
-#' @param x A data-frame like object passed to `[eval_select()]`,
-#'   `eval_rename()`, and friends.
+#' @param x A data-frame like object passed to [eval_select()],
+#'   [eval_rename()], and friends.
 #' @export
 tidyselect_data_proxy <- function(x) {
   UseMethod("tidyselect_data_proxy")
 }
 #' @export
-tidyselect_data_proxy.default <- function(x) x
+tidyselect_data_proxy.default <- function(x) {
+  x
+}
 
 
 #' @rdname tidyselect_data_proxy
