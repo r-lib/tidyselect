2e4e2daefe1ea58ccaa1726f0968f07a66c92c75
Lionel Henry <lionel.hry@gmail.com>
2024-10-28 14:08:27 +0100

    Apply suggestions from code review
    Co-authored-by: Davis Vaughan <davis@rstudio.com>

--- R/doc-tidy-selection.R
+++ R/doc-tidy-selection.R
@@ -6,7 +6,7 @@
 #' dialect of R for selecting variables based on their names or properties.
 #'
 #' Tidy selection is a variant of tidy evaluation. This means that inside
-#' functions, tidy-select arguments require special attention, as described in
+#' functions tidy-select arguments require special attention, as described in
 #' the *Indirection* section below. If you've never heard of tidy evaluation
 #' before, start with `vignette("programming")`.
 #'
@@ -21,12 +21,12 @@
 #'
 #' There are two main cases:
 #'
-#' *   If you have a character vector of column names, use `all_of()`
+#' *   If you want the user to supply a character vector of column names, use `all_of()`
 #'     or `any_of()`, depending on whether or not you want unknown variable
 #'     names to cause an error, e.g. `select(df, all_of(vars))`,
 #'     `select(df, !any_of(vars))`.
 #'
-#' *   If you want the user to be able to supply a tidyselect specification in
+#' *   If you want the user to supply a tidyselect specification in
 #'     a function argument, embrace the function argument, e.g.
 #'     `select(df, {{ vars }})`.
 #'
